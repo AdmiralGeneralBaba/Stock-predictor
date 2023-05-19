e36d95d6ebd577c44d467829dcaed1ef311a7418
Mio (Diyar) <37794647+MioLoveyah@users.noreply.github.com>
2023-05-19 16:15:04 +0100

    Removed getSymbolsBottom10thPercentile, looped sentiment

--- index.js
+++ index.js
@@ -12,90 +12,15 @@ const { time } = require("console");
 
 const configuration = new Configuration({
     organization: "org-BHaORzAJnznzo598IHG5xn2d",
-<<<<<<< Updated upstream
     apiKey: "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",
-=======
-    apiKey: "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",
->>>>>>> Stashed changes
 });
 const openai = new OpenAIApi(configuration);
 
 app.use(bodyParser.json()); 
 app.use(cors());
 
-// Cache variables
-let symbolsBottom10thPercentileCache;
-let symbolsBottom10thPercentileCacheTimestamp;
 
-async function getSymbolsBottom10thPercentile() {
-  const CACHE_VALIDITY = 60 * 60 * 1000; // 1 hour in milliseconds
-  const now = Date.now();
-
-  // If the cache is valid and not too old, return the cached data
-  if (symbolsBottom10thPercentileCache && now - symbolsBottom10thPercentileCacheTimestamp < CACHE_VALIDITY) {
-    return symbolsBottom10thPercentileCache;
-  }
-
-  // Otherwise, fetch the data and update the cache
-  try {
-    const response = await axios.get("https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=75E6A1OE5RSA3LIS");
-    const data = response.data;
-  
-    const rows = data.split('\n');
-    rows.shift();
-  
-    const symbols = rows.map(row => row.split(',')[0]);
-  
-    const marketCaps = [];
-    const BATCH_SIZE = 100;
-  
-    for (let i = 0; i < symbols.length; i += BATCH_SIZE) {
-      const batchSymbols = symbols.slice(i, i + BATCH_SIZE);
-      const url = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${batchSymbols.join(',')}`;
-      try {
-        const batchResponse = await axios.get(url);
-        const results = batchResponse.data.quoteResponse.result;
-  
-        results.forEach(result => {
-          marketCaps.push({ symbol: result.symbol, marketCap: result.marketCap });
-        });
-      } catch (error) {
-        console.error(`Error fetching market cap for ${batchSymbols.join(', ')}: ${error}`);
-      }
-    }
-  
-    const sortedMarketCaps = marketCaps.sort((a, b) => a.marketCap - b.marketCap);
-    const percentileIndex = Math.ceil(sortedMarketCaps.length * 0.1) - 1;
-    const percentile10th = sortedMarketCaps[percentileIndex].marketCap;
-  
-    const symbolsBottom10thPercentile = marketCaps.filter(({ marketCap }) => marketCap <= percentile10th)
-                                                   .map(({ symbol }) => symbol);
-
-    // Update the cache
-    symbolsBottom10thPercentileCache = symbolsBottom10thPercentile;
-    symbolsBottom10thPercentileCacheTimestamp = now;
-  
-    return symbolsBottom10thPercentile;
-  } catch (err) {
-    console.error("An error occurred: " + err);
-  }
-}
-
-
-
-app.post('/', async (req, res) => {
-  const { message } = req.body;
-
-  // try {
-  //   // Log the first 10 symbols from the bottom 10th percentile
-  //   const symbolsBottom10thPercentile = await getSymbolsBottom10thPercentile();
-  //   const symbolsBottom10thPercentileFirst10 = symbolsBottom10thPercentile.slice(0, 10);
-  //   console.log('First 10 symbols in the bottom 10th percentile:', symbolsBottom10thPercentileFirst10);
-  // } catch (error) {
-  //   console.error("An error occurred when fetching the bottom 10th percentile symbols: " + error);
-  // }
-
-  //Alpha vantage go:
+async function getSentiment(message) {
   const url = 'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers='+ message +'&limit=1&apikey=75E6A1OE5RSA3LIS';
 
   let headline = "";
@@ -133,13 +58,17 @@ app.post('/', async (req, res) => {
 
     console.log(openAIResponse.data.choices[0])
     if(openAIResponse.data.choices[0].text){
-          res.json({message: openAIResponse.data.choices[0].text + timepublished})
+          console.log({message: openAIResponse.data.choices[0].text + timepublished})
     }
   } catch (err) {
-    res.status(500).send("An error occurred: " + err);
+    console.log("An error occurred: " + err);
   }
-});
+}
 
+getSentiment("META");
+setInterval(() => {
+  getSentiment("META");
+}, 60 * 1000); // 60 * 1000 milliseconds = 1 minute
 
 
 app.get('/tickers', async (req, res) => {
